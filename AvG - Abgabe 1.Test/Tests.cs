ea666327aec325df6f830f8afa3e0e588616fa2a
michiiik <49305086+michiiik@users.noreply.github.com>
2019-04-06 12:52:03 +0200

    Veränderte sln und Testprojekt mit Beispieltest

---
+++ AvG - Abgabe 1.Test/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Entity;
+
+namespace AvG___Abgabe_1.Test
+{
+    [TestFixture]
+    public class Supplier_Construct
+    {
+        private readonly Supplier _supplier;
+
+        public Supplier_Construct()
+        {
+            _supplier = 
+                new Supplier("1", 
+                    "Test AG", 
+                    "test@test.de", 
+                    "015639812831", 
+                    "TestStrasse 3");
+        }
+        [Test]
+        public void ConstructObject()
+        {
+            var result = _supplier;
+            Assert.True(result is Supplier);
+        }
+    }
+    [TestFixture]
+    public class Product_Construct
+    {
+        private readonly Product _product;
+
+        public Product_Construct()
+        {
+            _product = new Product();
+        }
+
+        [Test]
+        public void ConstructObject()
+        {
+            var result = _product;
+            Assert.True(result is Product);
+        }
+    }
+}
